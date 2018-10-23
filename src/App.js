33de36d93e8f78b475f4eefd3423317a6c071514
Kaylee Bunce <kaybunce@hotmail.com>
2018-10-23 15:33:43 -0600

    Add Classes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Splash from './Splash';
+import Filter from './Filter';
+import Search from './Search';
+import CardContainer from './Card-Container';
 
-class App extends Component {
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Splash />
+        <Filter />
+        <Search />
+        <CardContainer />
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className='title'>
+          Fetch  
+        </h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
