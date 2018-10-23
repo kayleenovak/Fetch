9cac33ff0f2a881d57ccd8f25af9c55a2f76685d
Kaylee Bunce <kaybunce@hotmail.com>
2018-10-23 15:45:35 -0600

    Add content to filter class

--- src/Filter.js
+++ src/Filter.js
@@ -4,6 +4,48 @@ export default class Filter extends Component {
   render() {
     return (
       <div>
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>  
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>        
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>       
+        <div>
+          <input type='checkbox' id='male' />
+          <label for='male'>Male</label>
+          <input type='checkbox' id='female' />
+          <label for='female'>Female</label>
+        </div>
       </div>
     )
   }
