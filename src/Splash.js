8977827db5ee6b65a0698b0fab89dad119373681
Kaylee Bunce <kaybunce@hotmail.com>
2018-10-24 18:59:03 -0600

    Add styling to splash page

--- src/Splash.js
+++ src/Splash.js
@@ -4,12 +4,12 @@ import './Splash.css';
 export default class Splash extends Component {
   render() {
     return (
-      <div>
-        <img className='splash-logo logo' />
+      <div className="splash">
+        <h1 className='splash-title'>Fetch <i className="fas fa-dog"></i> </h1>
         <form>
-          <button>Fetch Dogs</button>
+          <button className='fetch-dogs-btn'>Fetch Dogs</button>
         </form>
-        <button>Show all dogs</button>
+        <button className='show-all-dogs-btn'>Show all dogs</button>
       </div>
     )
   }
