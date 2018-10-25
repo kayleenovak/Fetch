dd089db4ed40102f99999c20b45c49e940118c38
B3an5 <beantheo@gmail.com>
2018-10-25 13:07:04 -0600

    modal css and splash page sliders

--- src/Filter.js
+++ src/Filter.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import './Filter.css'
+
+
+
+
+
 
 export default class Filter extends Component {
+toggleModal = (event) => {
+  let breedModal = document.querySelector('.breed-sb');
+  event.target.parentElement.parentElement.classList.toggle('toggled-modal')
+}
   render() {
     return (
       <div>
+      <div className='sidebar breed-sb'>
+        <div className='caret-wrapper' onClick={this.toggleModal}>
+          <p className='caret'>Breed</p>
+        </div>
+        <h3 className='modal-title'>PLACE FILTER TITLE HERE</h3>
+        <p className='modal-body'> THIS IS WHERE CHECKBOXES AND RADIO BUTTONS SHOULD GO </p>
         
+      </div>      
+      <div className='sidebar gender-sb'>
+        <div className='caret-wrapper' onClick={this.toggleModal}>
+          <p className='caret'>Gender</p>
+        </div>
+        <h3 className='modal-title'>PLACE FILTER TITLE HERE</h3>
+        <p className='modal-body'> THIS IS WHERE CHECKBOXES AND RADIO BUTTONS SHOULD GO </p>
+        
+      </div>      
+      <div className='sidebar size-sb'>
+        <div className='caret-wrapper' onClick={this.toggleModal}>
+          <p className='caret'>Size</p>
+        </div>
+        <h3 className='modal-title'>PLACE FILTER TITLE HERE</h3>
+        <p className='modal-body'> THIS IS WHERE CHECKBOXES AND RADIO BUTTONS SHOULD GO </p>
+        
+      </div>
       </div>
     )
   }
--- src/Splash.js
+++ src/Splash.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import './Splash.css'
 
 export default class Splash extends Component {
   render() {
     return (
       <div>
-        <img className='splash-logo logo' />
-        <form>
-          <button>Fetch Dogs</button>
-        </form>
+        <img className='splash-logo logo' />        
+          <div className='splash-cont'>
+          <form>
+            <div className="switch">
+              <input className='radios' type="radio" name="choose1" id='male' value="Male" />
+              <input className='radios' type="radio" name="choose1" value="both" defaultChecked />
+              <input className='radios' type="radio" name="choose1" value="Female" />
+            </div>
+          </form>
+          <form>
+            <div className="switch">
+              <input className='radios' type="radio" name="choose2" value="Puppy" />
+              <input className='radios' type="radio" name="choose2" value="both" defaultChecked />
+              <input className='radios' type="radio" name="choose2" value="Adult" />
+            </div>
+          </form>
+          <form>
+            <div className="switch">
+              <input className='radios' type="radio" name="choose3" value="Big" />
+              <input className='radios' type="radio" name="choose3" value="both" defaultChecked />
+              <input className='radios' type="radio" name="choose3" value="Small" />
+            </div>
+          </form>
+          </div>
+          <button>Fetch Dogs</button>        
         <button>Show all dogs</button>
       </div>
     )
