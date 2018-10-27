29e7654b8091aed7d788e7f6671fb4f10481d991
Cody-Price <ctprice11@gmail.com>
2018-10-27 13:02:46 -0600

    Add name filter ability, and return to all dogs button functionality

--- src/Search.js
+++ src/Search.js
@@ -5,8 +5,9 @@ export default class Search extends Component {
   render() {
     return (
       <div>
-        <input type='search' />
-        <button>Search</button>
+        <input type='search' className='search-bar' />
+        <button onClick={ this.props.searchFilter }>Search</button>
+        <button onClick={ this.props.resetDogs }>Show All Dogs</button>
       </div>
     )
   }
